782f411fdadb1e8986ce1c2282443faca1c7b9e2
xiehui <1059248139@qq.com>
2021-11-23 17:42:58 +0800

    [添加]blackboard_behaviour说明

--- blackboard_behaviour.py
+++ blackboard_behaviour.py
@@ -3,36 +3,44 @@
 import py_trees
 
 
+# Foo 继承于 py_trees.behaviour.Behaviour
 class Foo(py_trees.behaviour.Behaviour):
 
     def __init__(self, name):
+        # 初始化父类,主要传入名字
         super().__init__(name=name)
+
+        # 创建三个 黑板 
         self.blackboard = self.attach_blackboard_client(name="Foo Global")
-        self.parameters = self.attach_blackboard_client(name="Foo Params", namespace="foo_parameters_")
-        self.state = self.attach_blackboard_client(name="Foo State", namespace="foo_state_")
+        self.parameters = self.attach_blackboard_client(name="Foo Params", namespace="foo/parameters/")
+        self.state = self.attach_blackboard_client(name="Foo State", namespace="foo/state/")
 
         # create a key 'foo_parameters_init' on the blackboard
         self.parameters.register_key("init", access=py_trees.common.Access.READ)
         # create a key 'foo_state_number_of_noodles' on the blackboard
         self.state.register_key("number_of_noodles", access=py_trees.common.Access.WRITE)
 
+    # init 
     def initialise(self):
         self.state.number_of_noodles = self.parameters.init
 
+    # 每次执行
     def update(self):
         self.state.number_of_noodles += 1
         self.feedback_message = self.state.number_of_noodles
+        # 大于 5 成功
         if self.state.number_of_noodles > 5:
             return py_trees.common.Status.SUCCESS
         else:
             return py_trees.common.Status.RUNNING
 
 
-# could equivalently do directly via the Blackboard static methods if
-# not interested in tracking / visualising the application configuration
+# 创建 黑板
 configuration = py_trees.blackboard.Client(name="App Config")
-configuration.register_key("foo_parameters_init", access=py_trees.common.Access.WRITE)
-configuration.foo_parameters_init = 3
+# 注册 foo/parameters/init
+configuration.register_key("foo/parameters/init", access=py_trees.common.Access.WRITE)
+# init 
+configuration.foo.parameters.init = 3
 
 foo = Foo(name="The Foo")
 for i in range(1, 8):
--- readme.md
+++ readme.md
@@ -1,1 +1,27 @@
 # py-trees-examples
+
+```shell
+blackboard_activity_stream.py
+
+# py-trees-demo-action-behaviour
+# # 展示典型动作行为特征
+blackboard_behaviour.py
+
+blackboard_disconnected.py
+blackboard_display.py
+blackboard_namespaces.py
+blackboard_nested.py
+blackboard_read_write.py
+chooser.py
+decorators.py
+oneshot.py
+parallel.py
+pickup_where_you_left_off.py
+readme.md
+selector.py
+selector_with_memory.py
+sequence.py
+sequence_with_memory.py
+skeleton_behaviour.py
+skeleton_tree.py
+```
